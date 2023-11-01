09da5e4bc4b77ac1f1c14289e7eb7d2fd967a9be
lear23 <130910243+lear23@users.noreply.github.com>
2023-11-01 13:46:51 +0100

    page not found ready

--- src/NotFoundSida/NotFoundSida.jsx
+++ src/NotFoundSida/NotFoundSida.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './NotFoundSida.css'
 import Dead from '../assets/images/grim.jpg';
-import Dog from '../assets/images/hund.jpg'
+// import Dog from '../assets/images/hund.jpg'
 import { Link } from 'react-router-dom';
 
 const NotFoundSida = () => {
@@ -10,12 +10,19 @@ const NotFoundSida = () => {
   };
 
   return (
-    <main>
-      <div className='container-bild'>
-        <img src={Dog} alt="Dead image" />
+    <main className='container'>
+      <div className='container-notfound'>
         <h2>404 Page Not Found</h2>
-        {/* <button className='' onClick={handleReturnClick}></button> */}
-        <Link className="btn-theme" onClick={handleReturnClick} to="/">Klicka Här För Att Återvända<i className="mindre fi fi-br-arrow-up-right"></i></Link>
+        <div className="dead">
+         
+           <img src={Dead} alt="Dead image" />
+        </div>
+        <div className="notfound">
+             {/* <h2>404 Page Not Found</h2> */}
+              {/* <button className='' onClick={handleReturnClick}></button> */}
+             <Link className="btn-theme" onClick={handleReturnClick} to="/">Klicka Här För Att Återvända<i className="mindre fi fi-br-arrow-up-right"></i></Link>
+        </div>
+        
       </div>
     </main>
   );
